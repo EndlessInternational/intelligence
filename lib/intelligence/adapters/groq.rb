8128678998a2481c5a07e23410c1cb1e8e6bd797
DavertMik <davert@testomat.io>
2025-05-15 21:29:31 +0300

    fixed tool choise schema

--- lib/intelligence/adapters/groq.rb
+++ lib/intelligence/adapters/groq.rb
@@ -4,12 +4,12 @@ module Intelligence
   module Groq
 
     class Adapter < Generic::Adapter
-      
+
       chat_request_uri 'https://api.groq.com/openai/v1/chat/completions'
 
-      schema do 
+      schema do
         key                   String
-        chat_options do 
+        chat_options do
           frequency_penalty   Float
           logit_bias
           logprobs            [ TrueClass, FalseClass ]
@@ -18,8 +18,8 @@ module Intelligence
           # the parallel_tool_calls is only allowed when 'tools' are specified
           parallel_tool_calls [ TrueClass, FalseClass ]
           presence_penalty    Float
-          response_format do 
-            # 'text' and 'json_object' are the only supported types; you must also instruct 
+          response_format do
+            # 'text' and 'json_object' are the only supported types; you must also instruct
             # the model to output json
             type              Symbol, in: [ :text, :json_object ]
           end
@@ -30,15 +30,8 @@ module Intelligence
             include_usage     [ TrueClass, FalseClass ]
           end
           temperature         Float
-          tool                array: true, as: :tools, &Tool.schema 
-          tool_choice do 
-            # one of 'auto', 'none' or 'function'
-            type              Symbol, in: [ :auto, :none, :function ]
-            # the function parameters is required if you specify a type of 'function' 
-            function do 
-              name            String 
-            end
-          end
+          tool                array: true, as: :tools, &Tool.schema
+          tool_choice         String
           top_logprobs        Integer
           top_p               Float
           user                String
