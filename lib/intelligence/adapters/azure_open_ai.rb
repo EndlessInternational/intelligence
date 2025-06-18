b077f76552db7245b62751f913af57552cc1f306
DavertMik <davert@testomat.io>
2025-06-19 00:11:25 +0300

    added microsoft azure auth by client/tenant

--- lib/intelligence/adapters/azure_open_ai.rb
+++ lib/intelligence/adapters/azure_open_ai.rb
@@ -1,4 +1,7 @@
 require_relative 'generic/adapter'
+require 'net/http'
+require 'uri'
+require 'json'
 
 module Intelligence
   module AzureOpenAi
@@ -8,6 +11,9 @@ module Intelligence
       schema do
         # Azure OpenAI specific properties
         key                         String
+        tenant_id                   String
+        client_id                   String
+        client_secret               String
         endpoint                    String, required: true
         api_version                 String, default: '2024-02-01'
         
@@ -58,6 +64,18 @@ module Intelligence
         end
       end
 
+      def fetch_azure_access_token(tenant_id, client_id, client_secret)
+        uri = URI("https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/token")
+        res = Net::HTTP.post_form(uri, {
+          'client_id' => client_id,
+          'client_secret' => client_secret,
+          'scope' => 'https://cognitiveservices.azure.com/.default',
+          'grant_type' => 'client_credentials'
+        })
+        raise res.body unless res.is_a?(Net::HTTPSuccess)
+        JSON.parse(res.body)['access_token']
+      end
+
       def chat_request_uri( options )
         options = merge_options( @options, build_options( options ) )
         endpoint = options[ :endpoint ]
@@ -75,16 +93,21 @@ module Intelligence
 
       def chat_request_headers( options = nil )
         options = merge_options( @options, build_options( options ) )
+
         result = {}
-
-        key = options[ :key ] 
-
-        raise ArgumentError.new( "An Azure OpenAI key is required to build an Azure OpenAI chat request." ) \
-          if key.nil?
-
+        tenant_id = options[ :tenant_id ]
+        client_id = options[ :client_id ]
+        client_secret = options[ :client_secret ]
+        if tenant_id && client_id && client_secret
+          token = fetch_azure_access_token(tenant_id, client_id, client_secret)
+          result[ 'Authorization' ] = "Bearer #{token}"
+        else
+          key = options[ :key ] 
+          raise ArgumentError.new( "An Azure OpenAI key is required to build an Azure OpenAI chat request." ) \
+            if key.nil?
+          result[ 'api-key' ] = key
+        end
         result[ 'Content-Type' ] = 'application/json'
-        result[ 'api-key' ] = key  # Azure uses 'api-key' instead of 'Authorization: Bearer'
-
         result 
       end
 
